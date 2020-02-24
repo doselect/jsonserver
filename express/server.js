57a05f72f525a0197751029db315040c4d55da5b
Vikramaditya <vk92kokil@live.com>
2020-02-24 11:57:51 +0530

    test

--- express/server.js
+++ express/server.js
@@ -1,21 +1,22 @@
 'use strict';
 const express = require('express');
 const serverless = require('serverless-http');
-const jsonServer = require('json-server');
+// const jsonServer = require('json-server');
 const request = require('request');
-const middlewares = jsonServer.defaults();
+// const middlewares = jsonServer.defaults();
 const app = express();
 
-app.use(middlewares);
-app.use('/.netlify/functions/json/:fileName', (req, res, next) => {
-   const { fileName } = req.params;
-   const opts = { url: `https://raw.githubusercontent.com/doselect/jsonserver/master/data/${fileName}.json`, json: true }
+// app.use(middlewares);
+app.get('/.netlify/functions/json', (req, res, next) => {
+   // const { fileName } = req.params;
+   const opts = { url: `https://raw.githubusercontent.com/doselect/jsonserver/master/data/db.json`, json: true }
    request(opts, function (error, response, body) {
       if (error) {
          return res.status(400).send({ error });
       }
-      const jsonrouter = jsonServer.router(body);
-      jsonrouter(req, res, next);
+      res.status(200).send(body);
+      // const jsonrouter = jsonServer.router(body);
+      // jsonrouter(req, res, next);
    });
 });
 
