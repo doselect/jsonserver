befd0ca5e351cbb349d87ed440e8ccf6594d136d
dinesh25murali <dinesh@doselect.com>
2020-03-03 16:49:51 +0530

    Modify: changed date format in leaves;

--- generate_data/leaves.js
+++ generate_data/leaves.js
@@ -10,22 +10,23 @@ Factory.define('leaves')
     .sequence('id')
     .attr('name', () => { return faker.name.firstName(); })
     .attr('startDate', () => {
-        const start = new Date(); 
+        const start = new Date();
         const end = new Date();
         end.setDate(start.getDate() + 2);
-        return faker.date.between(
+        const date = faker.date.between(
             start.getFullYear() + '-' + (start.getMonth() + 1) + '-' + start.getDate(),
-            end.getFullYear() + '-' + (end.getMonth() + 1) + '-' + end.getDate()
-        );
+            end.getFullYear() + '-' + (end.getMonth() + 1) + '-' + end.getDate());
+        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
     })
     .attr('endDate', () => {
-        const start = new Date(); 
+        const start = new Date();
         start.setDate(start.getDate() + 3);
         const end = new Date();
         end.setDate(start.getDate() + 4);
-        return faker.date.between(
+        const date = faker.date.between(
             start.getFullYear() + '-' + (start.getMonth() + 1) + '-' + start.getDate(),
             end.getFullYear() + '-' + (end.getMonth() + 1) + '-' + end.getDate());
+        return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
     });
 
 _.times(50, () => {
