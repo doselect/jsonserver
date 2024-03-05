07cdf274154ae6056cdce7cd799fa8d71cc1d9bc
Akshaykumar Bajaj <98586951+APB-DoSelect@users.noreply.github.com>
2024-03-05 14:01:49 +0530

    AWS deployment (#20)
    * Removed netlify related scripts and dependencies
    
    - Removed the netlify-lambda package and scripts related to it.
    - Updated all packages to remove vulnerabilities and checked working on Node v20.
    
    * Added logic so that the request works without adding filename twice.
    
    * Steps to run locally added in README.md

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# jsonserver
-DoSelect Json Server
+# DoSelect Json Server
+
+
+## Steps to run locally
+1. Clone the repo
+2. Install packages (npm install)
+3. Run command **npm run dev** to start the dev server
+4. To check if the server is working, open your web browser and go to <http://localhost:8888>. You should see the JsonServer welcome page. To check if data is returned as expected go to http://localhost:8888/api/filename where the filename is the name of a file from the **data** folder. e.g. <http://localhost:8888/api/notes> (Preferably use the URL in Postman for get request)
--- express/json.js
+++ express/json.js
@@ -1,23 +1,55 @@
-'use strict';
+"use strict";
 // const express = require('express');
-const serverless = require('serverless-http');
-const jsonServer = require('json-server');
-const request = require('request');
+const serverless = require("serverless-http");
+const jsonServer = require("json-server");
+const request = require("request");
 const middlewares = jsonServer.defaults();
 const app = jsonServer.create();
 
 app.use(middlewares);
-app.use('/.netlify/functions/json/:fileName', (req, res, next) => {
-   const { fileName } = req.params;
-   const opts = { url: `https://raw.githubusercontent.com/doselect/jsonserver/master/data/${fileName}.json`, json: true }
-   request(opts, function (error, response, body) {
-      if (error) {
-         return res.status(400).send({ error });
-      }
-      const jsonrouter = jsonServer.router(body);
-      jsonrouter(req, res, next);
-   });
+
+// Route to be hit by default
+app.use("/api/:fileName/:splat1?", (req, res, next) => {
+  const { fileName, splat1 } = req.params;
+  let redirectRoute, url;
+
+// Checking if splat exists and if yes, adding it to the end of the url   
+  if (splat1) {
+    url = `/${splat1}${req.url}`;
+  } else {
+    url = `${req.url}`;
+  }
+
+//   Checking if splat is equal to filename and if not adding filename twice to the route with url 
+  if (fileName === splat1) {
+    redirectRoute = `/json/${fileName}${url}`;
+  } else {
+    redirectRoute = `/json/${fileName}/${fileName}${url}`;
+  }
+
+//   Redirecting to the json route to fetch the data.
+  res.redirect(307,redirectRoute);
 });
 
+
+// Code to return data from jsonServer
+app.use("/json/:fileName/", (req, res, next) => {
+  const { fileName } = req.params;
+  const opts = {
+    url: `https://raw.githubusercontent.com/doselect/jsonserver/master/data/${fileName}.json`,
+    json: true,
+  };
+  request(opts, function (error, response, body) {
+    if (error) {
+      return res.status(400).send({ error });
+    }
+    const jsonrouter = jsonServer.router(body);
+    jsonrouter(req, res, next);
+  });
+});
+
+// App to run locally
 module.exports = app;
+
+// Handler to run as serverless
 module.exports.handler = serverless(app);
