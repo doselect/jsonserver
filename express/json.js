d413fe24267e4866c60729bdfce02c986f98db51
Akshaykumar Bajaj <98586951+APB-DoSelect@users.noreply.github.com>
2024-03-05 16:24:47 +0530

    Health check (#21)
    * Added health check at /health
    
    - Added the health endpoint using the express-actuator middleware.
    - Changed the variable splat1 to splat.
    
    * Added Health Check info to Readme
    
    * Removed middleware and added the health check route
    
    - Removed the express-actuator midddleware used for health check
    - Added route to ensure that files are fetched from git and then send status ok.

--- express/json.js
+++ express/json.js
@@ -1,5 +1,5 @@
 "use strict";
-// const express = require('express');
+const express = require('express');
 const serverless = require("serverless-http");
 const jsonServer = require("json-server");
 const request = require("request");
@@ -9,19 +9,19 @@ const app = jsonServer.create();
 app.use(middlewares);
 
 // Route to be hit by default
-app.use("/api/:fileName/:splat1?", (req, res, next) => {
-  const { fileName, splat1 } = req.params;
+app.use("/api/:fileName/:splat?", (req, res, next) => {
+  const { fileName, splat } = req.params;
   let redirectRoute, url;
 
 // Checking if splat exists and if yes, adding it to the end of the url   
-  if (splat1) {
-    url = `/${splat1}${req.url}`;
+  if (splat) {
+    url = `/${splat}${req.url}`;
   } else {
     url = `${req.url}`;
   }
 
 //   Checking if splat is equal to filename and if not adding filename twice to the route with url 
-  if (fileName === splat1) {
+  if (fileName === splat) {
     redirectRoute = `/json/${fileName}${url}`;
   } else {
     redirectRoute = `/json/${fileName}/${fileName}${url}`;
@@ -48,7 +48,21 @@ app.use("/json/:fileName/", (req, res, next) => {
   });
 });
 
-// App to run locally
+// Health Check
+app.use("/health", (req, res, next) => {
+  const opts = {
+    url: `https://raw.githubusercontent.com/doselect/jsonserver/master/data/health_check.txt`,
+    json: true,
+  };
+  request(opts, function (error, response, body) {
+    if (error) {
+      return res.status(503).send({ error });
+    }
+    return res.status(200).send({ status: "Ok" });
+  });
+});
+
+// App to run as node server
 module.exports = app;
 
 // Handler to run as serverless
